7ff8dce4dc5c6ba46efd7f9e8687b5dddf61edde
Javier Wilson <javier.wilson@gmail.com>
2016-11-20 03:11:41 -0600

    fixing file permissions

--- create-users.sh
+++ create-users.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 HOME=/wargame/home
+BIN=/wargame/bin
 for i in {0..9}; do
-	useradd level${i} -d $HOME/level${i}
+    id=$((2000+$i))
+    nexti=$(($i+1))
+	groupadd level${i} --gid $id
+	useradd level${i} -d $HOME/level${i} --uid $id --gid $id
+    rsync -av home/level${i}/ $HOME/level${i}/
+    chown -R level${i}:level${i} $HOME/level${i}/
+    chmod -R o-r $HOME/level${i}/
+    chown level${nexti}:level${nexti} $BIN/tmpserver${i}
+    chown level${nexti}:level${nexti} $BIN/tmpserver${i}.c
+    chown level${nexti}:level${nexti} $BIN/tmpserver${i}.sh
+    chmod +s $BIN/tmpserver${i}
 done
 chmod -t /tmp
